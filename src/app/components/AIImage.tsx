6d3722450e9d4bcf93fa13c83f271758b5aa5d5e
elie maatouk <emaatouk@hcbrands.com>
2025-05-22 14:48:42 -0400

    feat: enhance AI image generation UI and update global styles

--- src/app/components/AIImage.tsx
+++ src/app/components/AIImage.tsx
@@ -16,38 +16,44 @@ export default function AIImage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8">
-      <h1 className="text-3xl bg-gradient-to-r from-red-500 to-purple-400 bg-clip-text text-transparent">
-        Generate AI Image
-      </h1>
-      <textarea
-        placeholder="Please Type Prompt"
-        className="textarea textarea-primary max-w-1/2 w-1/2"
-        value={prompt}
-        onChange={(e) => setPrompt(e.target.value)}
-      ></textarea>
-      <button
-        className="btn btn-primary"
-        onClick={handleGenerate}
-        disabled={loading}
-      >
-        {loading ? "Generating..." : "Generate Image"}
-      </button>
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8 font-mono tracking-normal">
+      <div className="flex-1 flex justify-center items-center">
+        {!imageData && (
+          <h1 className="text-3xl bg-gradient-to-r from-start to-end bg-clip-text text-transparent text-center">
+            Generate AI Image
+          </h1>
+        )}
 
-      {loading ? (
-        <div className="skeleton card w-96 h-96"></div>
-      ) : (
-        imageData && (
-          <div className="card bg-base-100 w-96 shadow-md mt-4">
-            <figure>
-              <img
-                src={`data:image/png;base64,${imageData}`}
-                alt="Generated AI"
-              />
-            </figure>
-          </div>
-        )
-      )}
+        {loading ? (
+          <div className="skeleton card w-96 h-96"></div>
+        ) : (
+          imageData && (
+            <div className="card bg-base-100 w-96 shadow-md mt-4">
+              <figure>
+                <img
+                  src={`data:image/png;base64,${imageData}`}
+                  alt="Generated AI"
+                />
+              </figure>
+            </div>
+          )
+        )}
+      </div>
+      <div className="w-1/2 flex flex-row gap-2 mt-auto justify-center items-center">
+        <textarea
+          placeholder="Please Type Prompt"
+          className="textarea w-full border border-border rounded-3xl resize-none text-textMain"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        ></textarea>
+        <button
+          className="btn btn-primary rounded-4xl"
+          onClick={handleGenerate}
+          disabled={loading}
+        >
+          {loading ? "Generating..." : "Generate"}
+        </button>
+      </div>
     </div>
   );
 }
