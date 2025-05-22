d67eb212417f771f341e22f19c60924dd0a0c2e4
elie maatouk <emaatouk@hcbrands.com>
2025-05-22 15:02:10 -0400

    fix: improve loading state handling and update card styling in AI image component

--- src/app/components/AIImage.tsx
+++ src/app/components/AIImage.tsx
@@ -18,7 +18,7 @@ export default function AIImage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8 font-mono tracking-normal">
       <div className="flex-1 flex justify-center items-center">
-        {!imageData && (
+        {!imageData && !loading && (
           <h1 className="text-3xl bg-gradient-to-r from-start to-end bg-clip-text text-transparent text-center">
             Generate AI Image
           </h1>
@@ -28,21 +28,17 @@ export default function AIImage() {
           <div className="skeleton card w-96 h-96"></div>
         ) : (
           imageData && (
-            <div className="card bg-base-100 w-96 shadow-md mt-4">
-              <figure>
-                <img
-                  src={`data:image/png;base64,${imageData}`}
-                  alt="Generated AI"
-                />
-              </figure>
-            </div>
+            <img
+              className="w-full object-cover max-h-140"
+              src={`data:image/png;base64,${imageData}`}
+            />
           )
         )}
       </div>
       <div className="w-1/2 flex flex-row gap-2 mt-auto justify-center items-center">
         <textarea
           placeholder="Please Type Prompt"
-          className="textarea w-full border border-border rounded-3xl resize-none text-textMain"
+          className="textarea w-full border border-border rounded-3xl resize-none h-30 text-textMain p-5"
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
         ></textarea>
@@ -51,7 +47,7 @@ export default function AIImage() {
           onClick={handleGenerate}
           disabled={loading}
         >
-          {loading ? "Generating..." : "Generate"}
+          Generate
         </button>
       </div>
     </div>
